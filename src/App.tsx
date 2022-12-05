2ccc551807f1f7aeea94afeba6a7f273034c0133
IvanCastroRuizdevitech <ivan.castro@devitech.com.co>
2022-12-05 15:55:19 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+  <h1 className="text-3xl font-bold underline">
+    Centro Soporte!
+  </h1>
+  )
+}
+
+export default App
